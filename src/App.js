b12bf2e82b501d70bdff5e7ab97b0984d0438c71
dheeraj755148 <dheerajdayakaran95@gmail.com>
2021-01-18 00:29:35 +0530

    landing page--nav done ,[login, register](Routes created)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
+import { Switch, Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        {/*     <Route path="/login" exact component={Login} />
+    <Route path="/register" exact component={Register} /> */}
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Landin page on</h1>
+      </div>
+    </div>
+  );
+}
+export default Landing;
